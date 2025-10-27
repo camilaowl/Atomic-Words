47476eb2dd8880065a68beef14a1a9424ab3807e
Camila <camilaabalova@gmail.com>
2025-10-27 17:55:10 +0200

    some scc styles

--- lib/atomic_words_web/live/home_live.ex
+++ lib/atomic_words_web/live/home_live.ex
@@ -1,14 +1,38 @@
 defmodule AtomicWordsWeb.HomeLive do
   use Phoenix.LiveView
 
+  import AtomicWordsWeb.CoreComponents
+
   #alias AtomicWordsWeb.Router.Helpers, as: Routes
 
   def render(assigns) do
     ~H"""
-    <h1>Home</h1>
-    <button phx-click="go_to_settings">Go to Settings</button>
-    <button phx-click="go_to_stats">Go to Stats</button>
-    <button phx-click="words">Words</button>
+      <div class="flex flex-row justify-left p-5">
+        <div id="menu" class="mb-5" (style="margin-bottom: 20px;")>
+          <ul>
+            <li><button class="button" phx-click="go_to_settings">Go to Settings</button></li>
+            <li><button class="button" phx-click="go_to_stats">Go to Stats</button></li>
+            <li><button class="button" phx-click="words">Words</button></li>
+          </ul>
+        </div>
+
+        <div class="flex-col justify-center" style="margin-left: 100px;">
+          <div class="flex-row" style="position: relative;">
+            <div id="form" class="mt-5" style="margin-bottom: 20px; position: relative;">
+              <form class="add_word_form" style="display: flex; align-items: center;">
+                <input class="add_word_input" type="text" id="search" name="search" placeholder="Type to search words..."/>
+                <.icon name="hero-magnifying-glass" class="icon"/>
+              </form>
+            </div>
+
+          </div>
+
+          <div id="words-list" class="mt-5" style="margin-top: 20px;" >
+            <h1>Words List</h1>
+          </div>
+        </div>
+    </div>
+
     """
   end
 
