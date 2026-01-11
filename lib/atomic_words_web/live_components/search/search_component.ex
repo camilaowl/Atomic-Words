bc5401de58f1b943e453ffb2d1ac6473e373f229
camilaowl <camilaabalova@gmail.com>
2026-01-11 23:06:34 +0200

    lang switcher

--- lib/atomic_words_web/live_components/search/search_component.ex
+++ lib/atomic_words_web/live_components/search/search_component.ex
@@ -7,36 +7,46 @@ defmodule AtomicWordsWeb.LiveComponents.SearchComponent do
   def render(assigns) do
     ~H"""
     <div class="flex flex-col relative w-full">
-      <div
-        id="form"
-        class="my-2 relative w-full rounded-lg outline outline-black/5 dark:bg-grey-600 dark:shadow-none dark:-outline-offset-1 dark:outline-white/5 "
-      >
-        <form
-          phx-change="change"
-          phx-debounce="300"
+      <div class="flex flex-row items-center justify-between w-full my-2 gap-x-2">
+        <LangSwither.lang_swither
+          origin_lang={@origin_lang}
+          target_lang={@target_lang}
+          phx-click="switch_languages"
           phx-target={@myself}
-          class="flex flex-row items-center gap-1 w-full"
-          role="search"
+        />
+
+        <div
+          id="form"
+          class="relative w-full rounded-lg outline outline-black/5 dark:bg-grey-600 dark:shadow-none dark:-outline-offset-1 dark:outline-white/5 "
         >
-          <input
-            class=" px-4 w-full placeholder:italic display:inline bg-transparent focus:outline-none"
-            type="search"
-            id="search"
-            name="search"
-            placeholder="Type to search words..."
+          <form
+            phx-change="change"
             phx-debounce="300"
-          />
-          <button
-            type="button"
-            aria-label="Clear search"
-            phx-click="clear_search"
             phx-target={@myself}
-            class="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors flex items-center justify-center"
+            class="flex flex-row items-center gap-1 w-full"
+            role="search"
           >
-            <.icon name="hero-x-mark" class="w-5 h-5 text-gray-600 dark:text-gray-300" />
-          </button>
-        </form>
+            <input
+              class=" px-4 w-full placeholder:italic display:inline bg-transparent focus:outline-none"
+              type="search"
+              id="search"
+              name="search"
+              placeholder="Type to search words..."
+              phx-debounce="300"
+            />
+            <button
+              type="button"
+              aria-label="Clear search"
+              phx-click="clear_search"
+              phx-target={@myself}
+              class="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors flex items-center justify-center"
+            >
+              <.icon name="hero-x-mark" class="w-5 h-5 text-gray-600 dark:text-gray-300" />
+            </button>
+          </form>
+        </div>
       </div>
+
       <%= if Enum.empty?(@search_results) do %>
         <p class="text-sm text-gray-500">No results</p>
       <% else %>
@@ -69,6 +79,8 @@ defmodule AtomicWordsWeb.LiveComponents.SearchComponent do
       |> assign(:search_query, "")
       |> assign(:search_results, [])
       |> assign(:added_word_ids, added_word_ids)
+      |> assign(:origin_lang, "en")
+      |> assign(:target_lang, "uk")
 
     {:ok, socket}
   end
@@ -122,5 +134,17 @@ defmodule AtomicWordsWeb.LiveComponents.SearchComponent do
       end
 
     {:noreply, reply_socket}
+  end
+
+  def handle_event("switch_languages", _params, socket) do
+    origin_lang = socket.assigns.origin_lang
+    target_lang = socket.assigns.target_lang
+
+    socket =
+      socket
+      |> assign(:origin_lang, target_lang)
+      |> assign(:target_lang, origin_lang)
+
+    {:noreply, socket}
   end
 end
